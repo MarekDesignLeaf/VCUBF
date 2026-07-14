1378a9bb6344fbf0f71182f7429c079e1a3e8dc0
Marek <marek@designleaf.co.uk>
2026-07-14 15:29:05 +0100

    fix: make PWA client refresh resilient

--- frontend/public/sw-force-refresh.js
+++ frontend/public/sw-force-refresh.js
@@ -1,7 +1,7 @@
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     self.clients.matchAll({ type: "window" }).then((clients) =>
-      Promise.all(clients.map((client) => client.navigate(client.url)))
+      Promise.allSettled(clients.map((client) => client.navigate(client.url)))
     )
   );
 });
